1e4fe3989f0c00780619ab7590b0a8cd1fb0c69c
Hanna Murai <annmuray75@gmail.com>
2020-06-22 11:10:21 -0500

    changes4

---
+++ README.md/README.md
@@ -0,0 +1,11 @@
+
+Candy shop” is a brain training game designed to improve math skills, it was created  by using canvas and DOM manipulation technologies
+Benefits:
+- [ ] Become more confident using mental math
+- [ ] Learn targeted strategies to do math more quickly 
+How to play the game?
+1. Solve the simple math exercise 
+2. Click on a candy with the right result
+3. Do as many as possible tasks for certain time(90mins)
+
+You can find the game here: https://murai1998.github.io/CandyShop/
--- script.js
+++ script.js
@@ -1,237 +1,252 @@
-const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d');
-let id = null;
-let frames = 1;
-let hearts = document.querySelectorAll('#heart');
-let timer = 90;
+let start = document.querySelector('#start');
+let finish = document.querySelector('#finish');
+start.onclick = gameStart;
+finish.onclick = () => {
 
-
-
-let shopImg = new Image()
-let cartImage = new Image();
-let candy1 = new Image();
-let candy2 = new Image();
-let candy3 = new Image();
-let candy4 = new Image();
-let name = [candy1, candy2, candy3, candy4]
-shopImg.src = 'Images/CandyShop.jpg'
-cartImage.src = 'Images/backG.png';
-candy1.src = 'Images/candy3.png'
-candy2.src = 'Images/candy1.png'
-candy3.src = 'Images/candy2.png'
-candy4.src = 'Images/candy4.png'
-
-shopImg.onload = animate;
-cartImage.onload = animate;
-candy1.onload = animate;
-candy2.onload = animate;
-candy3.onload = animate;
-candy4.onload = animate;
-//-----Class Game---
-class Game {
-    constructor({
-        candies,
-        frames,
-        id,
-        score,
-        max,
-        min
-    }) {
-        this.candies = candies
-        this.frames = frames
-        this.id = id
-        this.score = score
-        this.max = max
-        this.min = min
-        this.lives = 3
-        this.level = 1;
-    }
-    createCandies = () => {
-        this.candies = [];
-        let num1 = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-        let num2 = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-        let index = Math.floor(Math.random() * operators.length) % operators.length;
-        let sign = operators[index].sign;
-        let result = operators[index].result(num1, num2);
-        this.equipment = `${num1} ${sign} ${num2}`;
-        let results = [];
-        results.push(result);
-        results.push(result - 1);
-        results.push(result + 1);
-        results.push(result - 2);
-        results = shuffle(results);
-        this.result = result;
-        for (let i = 0, length = 5; i < name.length; i++) {
-
-            let obj = new Candy({
-                img: name[i],
-                x: length,
-                y: -135,
-                width: 135,
-                height: 135,
-                num: results[i]
-            });
-            this.candies.push(obj);
-            if (i === 1) length += 230
-            length += 140
-        }
-    }
-    drawCandies = () => this.candies.forEach(candy => candy.drawCandy());
-    removeCandy = () => this.candies.forEach((candy, i) => {});
 }
 
+function gameStart() {
+    const canvas = document.querySelector('#canvas');
+    const ctx = canvas.getContext('2d');
+    let id = null;
+    let frames = 1;
+    let hearts = document.querySelectorAll('#heart');
+    let timer = 90;
+
+    let shopImg = new Image()
+    let cartImage = new Image();
+    let candy1 = new Image();
+    let candy2 = new Image();
+    let candy3 = new Image();
+    let candy4 = new Image();
+    let music = new Audio();
+
+    let name = [candy1, candy2, candy3, candy4]
+    shopImg.src = 'Images/CandyShop.jpg'
+    cartImage.src = 'Images/backG.png';
+    candy1.src = 'Images/candy3.png'
+    candy2.src = 'Images/candy1.png'
+    candy3.src = 'Images/candy2.png'
+    candy4.src = 'Images/candy4.png'
+    music.src = 'Images/musicBack.mp3'
+
+    shopImg.onload = animate;
+    cartImage.onload = animate;
+    candy1.onload = animate;
+    candy2.onload = animate;
+    candy3.onload = animate;
+    candy4.onload = animate;
+    music.onload = animate;
 
 
-//-----class Candy----
-class Candy {
-    constructor({
-        img,
-        x,
-        y,
-        width,
-        height,
-        num
-    }) {
-        this.img = img;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.num = num;
-    }
-    drawCandy = () => {
-        this.y += 0.1;
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
-        ctx.fillText(this.num, this.x + 34, this.y + 70);
-        ctx.style
-
-    }
-}
-
-
-
-let max = 100; //will change later for different leves, It will go to the Game class
-let min = 0;
-let operators = [{
-        sign: '+',
-        result: (num1, num2) => {
-            return num1 + num2
+    //-----Class Game---
+    class Game {
+        constructor({
+            candies,
+            frames,
+            id,
+            score,
+            max,
+            min
+        }) {
+            this.candies = candies
+            this.frames = frames
+            this.id = id
+            this.score = score
+            this.max = max
+            this.min = min
+            this.lives = 3
+            this.level = 1;
         }
-    },
-    {
-        sign: '-',
-        result: (num1, num2) => {
-            return num1 - num2
-        }
-    },
-    {
-        sign: '*',
-        result: (num1, num2) => {
-            return num1 * num2
-        }
-    }
-];
+        createCandies = () => {
+            this.candies = [];
+            let num1 = Math.floor(Math.random() * (this.max - this.min)) + this.min;
+            let num2 = Math.floor(Math.random() * (this.max - this.min)) + this.min;
+            let index = Math.floor(Math.random() * operators.length) % operators.length;
+            let sign = operators[index].sign;
+            let result = operators[index].result(num1, num2);
+            this.equipment = `${num1} ${sign} ${num2}`;
+            let results = [];
+            results.push(result);
+            results.push(result - 1);
+            results.push(result + 1);
+            results.push(result - 2);
+            results = shuffle(results);
+            this.result = result;
+            for (let i = 0, length = 5; i < name.length; i++) {
 
-let game = new Game({
-    candies: [],
-    frames: 1,
-    id: null,
-    score: 0,
-    max: 100,
-    min: 0
-});
-//----Remove candies----
-canvas.addEventListener('mousedown', function (e) {
-    const rect = canvas.getBoundingClientRect()
-    const x = event.clientX - rect.left
-    const y = event.clientY - rect.top
-    game.candies.forEach(candy => {
-        if (candy.x <= x && x <= candy.x + candy.width && candy.y <= y && y <= candy.y + candy.height) {
-            if (candy.num === game.result) {
-                game.createCandies();
-            } else {
-                hearts[game.lives - 1].remove()
-                game.lives -= 1;
-                if (game.lives === 0) {
-                    game.candies = [];
-                    clearInterval(id);
-                    erase();
-                } else {
-                    alert(`Oops! Wrong answer!`);
-                    game.createCandies();
-                }
-
+                let obj = new Candy({
+                    img: name[i],
+                    x: length,
+                    y: -135,
+                    width: 135,
+                    height: 135,
+                    num: results[i]
+                });
+                this.candies.push(obj);
+                if (i === 1) length += 230
+                length += 140
             }
-            console.log({
-                x,
-                y
-            });
         }
+        drawCandies = () => this.candies.forEach(candy => candy.drawCandy());
+        removeCandy = () => this.candies.forEach((candy, i) => {});
+    }
+
+
+
+    //-----class Candy----
+    class Candy {
+        constructor({
+            img,
+            x,
+            y,
+            width,
+            height,
+            num
+        }) {
+            this.img = img;
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.num = num;
+        }
+        drawCandy = () => {
+            this.y += 0.1;
+            ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+            ctx.fillText(this.num, this.x + 34, this.y + 70);
+            ctx.style
+
+        }
+    }
+
+
+
+    let max = 100; //will change later for different leves, It will go to the Game class
+    let min = 0;
+    let operators = [{
+            sign: '+',
+            result: (num1, num2) => {
+                return num1 + num2
+            }
+        },
+        {
+            sign: '-',
+            result: (num1, num2) => {
+                return num1 - num2
+            }
+        },
+        {
+            sign: '*',
+            result: (num1, num2) => {
+                return num1 * num2
+            }
+        }
+    ];
+
+    let game = new Game({
+        candies: [],
+        frames: 1,
+        id: null,
+        score: 0,
+        max: 100,
+        min: 0
     });
-});
+    //----Remove candies----
+    canvas.addEventListener('mousedown', function (e) {
+        const rect = canvas.getBoundingClientRect()
+        const x = event.clientX - rect.left
+        const y = event.clientY - rect.top
+        game.candies.forEach(candy => {
+            if (candy.x <= x && x <= candy.x + candy.width && candy.y <= y && y <= candy.y + candy.height) {
+                if (candy.num === game.result) {
+                    game.createCandies();
+                } else {
+                    hearts[game.lives - 1].remove()
+                    game.lives -= 1;
+                    if (game.lives === 0) {
+                        game.candies = [];
+                        music.pause();
+                        clearInterval(id);
+                        erase();
+                    } else {
+                        alert(`Oops! Wrong answer!`);
+                        game.createCandies();
+                    }
 
-function drawShop() {
-    ctx.drawImage(shopImg, 0, 0, canvas.width, canvas.height);
-    ctx.drawImage(cartImage, canvas.width / 2 - 160, canvas.height / 2 + 68, 290, 140);
-}
+                }
+                console.log({
+                    x,
+                    y
+                });
+            }
+        });
+    });
 
-//---Random array---
-function shuffle(array) {
-    var currentIndex = array.length,
-        temporaryValue, randomIndex;
-    while (0 !== currentIndex) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+    function drawShop() {
+        ctx.drawImage(shopImg, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(cartImage, canvas.width / 2 - 160, canvas.height / 2 + 68, 290, 140);
     }
-    return array;
-}
 
-
-//-----Animation-----
-function animate() {
-    window.requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawShop();
-
-    ctx.font = "45px bolder Stencil Std, fantasy";
-    ctx.fillStyle = 'white';
-    ctx.strokeStyle = 'white'
-    ctx.strokeText(game.equipment, canvas.width / 2 - 72, canvas.height / 2 + 145);
-
-    if (game.candies[0].y >= canvas.height) {
-        hearts[game.lives - 1].remove()
-        game.lives -= 1;
-
-
-        if ((game.lives === 0) && (timer === 0)) {
-            game.candies = [];
-            clearInterval(id);
-            erase();
-        } else {
-            alert(`Oops! Wrong answer!`);
-            game.createCandies();
+    //---Random array---
+    function shuffle(array) {
+        var currentIndex = array.length,
+            temporaryValue, randomIndex;
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
         }
+        return array;
     }
-    game.drawCandies();
-    game.removeCandy();
 
-};
-//----Timer----
-id = setInterval(function () {
-    document.querySelector('#timer span').innerHTML = timer
-    timer--;
-    if (timer === 0 || game.lives === 0) {
-        document.querySelector('#timer').innerHTML = '0'
-        document.querySelector('#timer').remove();
-    }
-}, 1000)
 
-//----Create candies----
-setTimeout(function () {
-    id = window.requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    game.createCandies();
-}, 10);
+    //-----Animation-----
+    function animate() {
+        window.requestAnimationFrame(animate);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawShop();
+        music.play()
+        ctx.font = "45px bolder Stencil Std, fantasy";
+        ctx.fillStyle = 'white';
+        ctx.strokeStyle = 'white'
+        ctx.strokeText(game.equipment, canvas.width / 2 - 72, canvas.height / 2 + 145);
+
+        if (game.candies[0].y >= canvas.height) {
+            hearts[game.lives - 1].remove()
+            game.lives -= 1;
+
+
+            if ((game.lives === 0) && (timer === 0)) {
+                game.candies = [];
+                music.pause();
+                clearInterval(id);
+                erase();
+            } else {
+                alert(`Oops! Wrong answer!`);
+                game.createCandies();
+            }
+        }
+        game.drawCandies();
+        game.removeCandy();
+
+    };
+    //----Timer----
+    id = setInterval(function () {
+        document.querySelector('#timer span').innerHTML = timer
+        timer--;
+        if (timer === 0 || game.lives === 0) {
+            document.querySelector('#timer').innerHTML = '0'
+            document.querySelector('#timer').remove();
+        }
+    }, 1000)
+
+    //----Create candies----
+    setTimeout(function () {
+        id = window.requestAnimationFrame(animate);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.createCandies();
+    }, 10);
+}
